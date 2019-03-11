f1df48cd0c0dd86880b82507d7e94dc3650eb39c
riddhi parmar <riddhi.parmar@kevit.io>
2019-03-11 15:20:52 +0530

    Add USER SignUp,Signin,Update Data

--- model/user.js
+++ model/user.js
@@ -5,13 +5,19 @@ const _= require('lodash')
 const bcrypt =require('bcryptjs')
 
 var userschema = new mongoose.Schema({
-    Name:{
-        type:String,
-        required:true,
-    },
-    Address:{
+    name:{
         type:String,
         required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true,
+        minlength:10,
+        maxlength:10
     },
     email:{
         type:String,
@@ -24,10 +30,6 @@ var userschema = new mongoose.Schema({
             message:'{value}is not valid email'
         }
 
-    },
-    Phone:{
-           type:Number,
-           required:true
     },
     password:{
         type:String,
@@ -47,5 +49,89 @@ var userschema = new mongoose.Schema({
         }
     }]
 })
+
+
+userschema.methods.toJSON = function()
+{
+    var user = this
+    var userObject  = user.toObject()
+    return _.pick(userObject,['_id','name','address','phone','email'])
+}
+userschema.methods.generateAuthToken = function()
+{
+    var user = this
+    var access ='auth'
+    var token =jwt.sign({_id: user._id.toHexString(),access},'udata66').toString()
+    user.tokens.push({access,token})
+    return user.save().then(()=>
+    {
+        return token
+    })
+}
+userschema.statics.findByToken =function(token)
+{
+var user = this
+var decode;
+try{ 
+
+   decode = jwt.verify(token,'udata66')
+}
+catch(e)
+{
+   return Promise.reject()
+}
+return user.findOne(
+    {
+        '_id':decode._id,
+        'tokens.token':token,
+        'tokens.access':'auth'
+
+    })
+}
+
+userschema.pre('save',function(next)
+{
+    var user = this
+    if(user.isModified('password'))
+    {
+         bcrypt.genSalt(10,(err,salt)=>
+         {
+             bcrypt.hash(user.password,salt,(err,hash)=>
+             {
+                 user.password =hash     
+                 next()
+             })
+         })
+    }
+    else{
+        next()
+    }
+})
+ userschema.statics.findByCredentials = function(email,password)
+ {
+     var user = this
+
+     return user.findOne({email}).then((user)=>
+     {
+         if(!user)
+         {
+             return Promise.reject()
+         }
+         return new Promise((resolve,reject) =>
+         {
+             bcrypt.compare(password,user.password,(err,res)=>
+             {
+                 if(res)
+                 {
+                     resolve(user)
+                 }
+                 else{
+                     reject()
+                 }
+             })
+         })
+     })
+ }
+
 var ud = mongoose.model('Userdata',userschema)
 module.exports = {ud}
--- server.js
+++ server.js
@@ -2,27 +2,69 @@ const express=require('express');
 const bodyparse =require('body-parser')
 const _=require('lodash');
 const {ObjectId}=require('mongodb')
-
-const {mongoose}=require('./data-connection/connection')
+const {mongoose}=require('./db/datacon')
 const {ud} =require('./model/user')
-const {authenticate}=require('./middleware/authenticate')
-
+const{authenticate}=require('./middleware/authenticate')
 var app =express();
 app.use(bodyparse.json())
-app.post('/Udata',(req,res)=>
+
+app.post('/User',(req,res)=>
 {
-   var udata = new ud({
-           Name:req.body.Name,
-           Address:req.body.Address,
-           email:req.body.email,
-           Phone:req.body.phone,
-           Password:req.body.passowrd
-       })
-       udata.save().then((data)=>
-       {
-            res.send(data)
-       },(e)=>
-       {
-           res.status(400).send(e);
-       })
+    var body =_.pick(req.body,['name','address','phone','email','password'])
+    var udata = new ud(body)
+    udata.generateAuthToken().then((result)=>
+    {
+        res.header('x-auth',result).send(udata)
+
+    }).catch((e)=>
+    {
+        console.log(e)
+         res.status(400).send(e)
+    })
+
+})
+
+app.get('/user/me',authenticate,(req,res)=>
+{
+    res.send(req.user)
+
+})
+app.post('/user/login',authenticate,(req,res)=>{
+
+    var body =_.pick(req.body,['email','password'])
+    
+    ud.findByCredentials(body.email,body.password).then((user)=>
+    {
+        res.send(req.user)
+    }).catch((e)=>{
+
+        res.status(400).send()
+    })
+    
+})
+app.patch('/user/:id',authenticate,(req,res)=>
+{
+    var id = req.params.id  
+    console.log(id)
+    var body = _.pick(req.body,['name','address','phone','email','password'])
+    if(!ObjectId.isValid(id))
+    {
+         return res.status(400).send();
+    }
+    ud.findByIdAndUpdate(id,{$set:body},{new:true}).then((udata)=>
+    {
+         if(!udata)
+         {
+             return res.status(404).send()
+         }
+         res.send({udata})
+    }).catch((e)=>
+    {
+        res.status(400).send()
+    })
+})
+
+app.listen(6612,()=>
+{
+    console.log('work on port number 6600')
 })
